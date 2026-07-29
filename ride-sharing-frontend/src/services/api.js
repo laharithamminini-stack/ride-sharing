f577f88d76a16c15fb1fc5190407bb110b6c3ec5
Lakshmi Priya <lakshmipriya@gmail.com>
2026-07-29 12:17:49 +0530

    Update API URL to Render backend

--- ride-sharing-frontend/src/services/api.js
+++ ride-sharing-frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://ride-sharing-backend-r310.onrender.com",
 });
 
 export default api;
