dd1fc363a438342f2cf6efee2ff40655191c6844
Lakshmi Priya <lakshmipriya@gmail.com>
2026-07-29 12:18:43 +0530

    Replace localhost with Render URL

--- ride-sharing-frontend/src/services/socket.js
+++ ride-sharing-frontend/src/services/socket.js
@@ -1,5 +1,6 @@
+
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://ride-sharing-backend-r310.onrender.com");
 
 export default socket;
